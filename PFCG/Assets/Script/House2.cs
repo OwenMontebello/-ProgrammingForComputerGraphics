edbde199d5e733b7597b83e4f8681c266a454a8c
OwenMontebello <owen.montebello.d57546@mcast.edu.mt>
2024-01-06 18:05:38 +0100

    Level2Update
    Added new houses
    Added a new spawner so i can spawn 2 different houses

---
+++ PFCG/Assets/Script/House2.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House2 : MonoBehaviour
+{
+    void Start()
+    {
+       GameObject leftWall = new GameObject();
+       leftWall.name = "Left Wall";
+       leftWall.AddComponent<Wall2>();
+       leftWall.transform.position = this.transform.position;
+       leftWall.transform.parent = this.transform;
+
+       GameObject rightWall = new GameObject();
+       rightWall.name = "Right Wall";
+       rightWall.AddComponent<Wall2>();
+       rightWall.transform.position = new Vector3(this.transform.position.x,
+                                                  this.transform.position.y,
+                                                  this.transform.position.z + 100);
+       rightWall.transform.parent = this.transform;
+
+
+       GameObject frontWall = new GameObject();
+       frontWall.name = "Front Wall";
+       frontWall.AddComponent<Wall2>();
+       frontWall.transform.position = new Vector3(this.transform.position.x + 49,
+                                                  this.transform.position.y,
+                                                  this.transform.position.z + 50);
+       frontWall.transform.Rotate(0, 90f, 0, Space.Self);
+       frontWall.transform.parent = this.transform;      
+
+       GameObject backWall = new GameObject();
+       backWall.name = "Back Wall";
+       backWall.AddComponent<Wall2>();
+       backWall.transform.position = new Vector3(this.transform.position.x - 49,
+                                                  this.transform.position.y,
+                                                  this.transform.position.z + 50);
+       backWall.transform.Rotate(0, 90f, 0, Space.Self);
+       backWall.transform.parent = this.transform;   
+    
+       GameObject floor = new GameObject();
+       floor.name = "Floor";
+       floor.AddComponent<Wall>();
+       floor.transform.position = new Vector3(this.transform.position.x,
+                                              this.transform.position.y - 20,
+                                              this.transform.position.z + 50);
+       floor.transform.Rotate(90f, 0 ,0, Space.Self);
+       floor.transform.localScale = new Vector3(this.transform.localScale.x,
+                                                this.transform.localScale.y + 1.5f,
+                                                this.transform.localScale.z);
+       floor.transform.parent = this.transform;
+
+       GameObject roof = new GameObject();
+       roof.name = "Roof";
+       roof.AddComponent<Wall>();
+       roof.transform.position = new Vector3(this.transform.position.x,
+                                              this.transform.position.y + 199.1f,
+                                              this.transform.position.z + 50);
+       roof.transform.Rotate(90f, 0 ,0, Space.Self);
+       roof.transform.localScale = new Vector3(this.transform.localScale.x,
+                                                this.transform.localScale.y + 1.5f,
+                                                this.transform.localScale.z);
+       roof.transform.parent = this.transform;
+    }
+
+}
---
+++ PFCG/Assets/Script/Wall2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall2 : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 WallSize = new Vector3(50, 200, 1);
+
+    void Start()
+    {
+       GameObject cubeGameObject = new GameObject();
+       cubeGameObject.name = "Cube";
+       Cube cube = cubeGameObject.AddComponent<Cube>();
+       cubeGameObject.GetComponent<Cube>().SetSize(WallSize);
+       cubeGameObject.transform.position = this.transform.position;
+       cubeGameObject.transform.rotation = this.transform.rotation;
+       cubeGameObject.transform.localScale = this.transform.localScale;
+       cubeGameObject.transform.parent = this.transform;
+    }
+}
